a41834d9a0904352d0a1774acdbb9ae04838cd20
rea <hi@rea.ink>
2023-01-13 13:31:56 +0800

    fix: chadrc

--- chadrc.lua
+++ chadrc.lua
@@ -2,22 +2,24 @@ local M = {}
 
 local highlights = require "custom.highlights"
 
-M.plugins = require "custom.plugins"
-
--- M.options = {
---   nvChad = {
---     update_branch = "dev",
---   },
--- }
+M.options = {
+  nvChad = {
+    update_url = "https://github.com/NvChad/NvChad",
+    update_branch = "main",
+  },
+}
 
 M.ui = {
-  theme = "everforest",
-  transparency = false,
-  theme_toggle = { "everforest", "everforest_light" },
-
+  -- hl = highlights
   hl_add = highlights.add,
   hl_override = highlights.override,
+  changed_themes = {},
+  theme_toggle = { "everforest", "everforest_light" },
+  theme = "everforest", -- default theme
+  transparency = false,
 }
+
+M.plugins = require "custom.plugins"
 
 M.mappings = require "custom.mappings"
 
