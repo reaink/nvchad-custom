968590b75678daef8381381e5b2798efdd2d0dd1
rea-work <rea-work@ReaWork.local>
2023-03-14 13:18:33 +0800

    settle conflict

--- plugins.lua
+++ plugins.lua
@@ -317,6 +317,25 @@ local plugins = {
   },
 
   {
+    "zegervdv/settle.nvim",
+    opt = true,
+    cmd = { "SettleInit" },
+    config = function()
+      require("settle").setup {
+        wrap = true,
+        symbol = "!",
+        keymaps = {
+          next_conflict = "-n",
+          prev_conflict = "-N",
+          use_ours = "-u1",
+          use_theirs = "-u2",
+          close = "-q",
+        },
+      }
+    end,
+  },
+
+  {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
     config = function()
