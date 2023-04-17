0577c0150562f7981681c9e1499da46b408c8cfc
rea-work <rea-work@ReaWork.local>
2023-04-17 16:51:55 +0800

    fix: illuminate

--- plugins.lua
+++ plugins.lua
@@ -280,6 +280,22 @@ local plugins = {
   -- automatically highlighting other uses of the word under the cursor
   {
     "RRethy/vim-illuminate",
+    lazy = true,
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = { "CursorMoved", "InsertLeave" },
+    config = function()
+      require("illuminate").configure {
+        filetypes_denylist = {
+          "neotree",
+          "neo-tree",
+          "Telescope",
+          "telescope",
+        },
+      }
+    end,
   },
 
   -- github plugin
